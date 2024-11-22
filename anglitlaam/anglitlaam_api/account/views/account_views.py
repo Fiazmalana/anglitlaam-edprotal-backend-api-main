e2bdcb35ffd5279396187a2809cca1548299429f
Fiazmalana <fiazmalana193@gmail.com>
2024-11-22 17:14:22 +0500

    filters addfro search

--- anglitlaam/anglitlaam_api/account/views/account_views.py
+++ anglitlaam/anglitlaam_api/account/views/account_views.py
@@ -1,7 +1,12 @@
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from ..models.portal_account_models import Account
 from ..serializers.portal_account_serializers import AccountSerializer
+
 class AccountViewSet(viewsets.ModelViewSet):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
-
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = '__all__'  # Enable filtering on all fields
+    search_fields = '__all__'    # Enable search across all fields
--- anglitlaam/anglitlaam_api/anglitlaam_api/settings.py
+++ anglitlaam/anglitlaam_api/anglitlaam_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'account',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
